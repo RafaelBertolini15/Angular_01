6cc502ffb907d5873676f4068a19ad7657488e03
brtl15 <rafaelhbertolini@gmail.com>
2025-10-22 20:40:21 -0300

    adicionado crud angular material

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 ### Explicando repositório
 
-Este repositório **serve para guardar** os primeiros contatos em que tive com o **Angular**. Foram feitos **seguindo passos** de um curso pela plataforma *Udemy*.
+Este repositório **serve para guardar** as primeiras aplicações em que tive com o **Angular**. Foram feitos **seguindo passos** de um curso pela plataforma *Udemy*.
 
 [Link para o curso](https://www.udemy.com/share/10d90N3@6opGk50Z0cS0ZpjGNsJFTJG8xKDym2PUvj9BwfIznBGR43cxZePqFHPudvcWtmVaxw==/)
+
+## Conceitos básicos
+
+Na pasta de conceitos básicos, você encontra uma **calculadora** que executa somente **somas**, e também encontra uma **lista de compras**, nesses dois aprendo o básico do angular. Um **resumo do aprendizado** pode ser descrito: 
+
+> Renderização de condicional com IF, capturar valores de formulários utilizando-se de módelos Forms do Angular, aprendo a trabalhar com submit de formulário, fazer um formulário de adicção de itens, trabalhar com classes e modelos de arrays, aprendo a usar @for e ngFor para renderizar componentes de lista, e aprendo a lógica de riscar itens e classes de forma dinaminca no componente.
+
---
+++ crud-angular-material/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,102 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatCard, MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { FormsModule } from '@angular/forms'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { MatSelectChange, MatSelectModule } from '@angular/material/select'
+import { MatButtonModule } from '@angular/material/button'
+import { Cliente } from './cliente';
+import { ClienteService } from '../cliente.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask'
+import { BrasilapiService } from '../brasilapi.service';
+import {Estado, Municipio} from '../brasilapi.models'
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [FlexLayoutModule, MatCardModule,
+    FormsModule, MatFormFieldModule, MatSelectModule,
+    MatInputModule, MatIconModule, MatButtonModule,
+    CommonModule,
+  NgxMaskDirective], providers: [ 
+    provideNgxMask()
+  ],
+
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss'
+})
+export class CadastroComponent implements OnInit {
+
+  cliente: Cliente = Cliente.newCliente();
+  atualizando: boolean = false;
+  snack: MatSnackBar = inject(MatSnackBar);
+  estados: Estado[] = [];
+  municipios : Municipio []= [];
+ 
+  constructor(
+    private service: ClienteService,
+     private brasilApiService: BrasilapiService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParamMap.subscribe
+      ((query: any) => {
+        const params = query['params']
+        const id = params['id']
+        if (id) {
+          let clienteEncontrado = this.service.buscarClientePorId(id);
+
+          if (clienteEncontrado) {
+            this.atualizando = true;
+            this.cliente = clienteEncontrado;
+            if(this.cliente.uf){
+              this.carregarMunicipios(this.cliente?.uf);
+            }
+          }
+        }
+      })
+
+      this.carregarUFs();
+  }
+
+carregarUFs(){
+  this.brasilApiService.listarUFs().subscribe({
+    next: listaEstados => {
+      console.log("lista estados", listaEstados);
+      this.estados = listaEstados; 
+    },
+    error: erro => console.log("erro ao carregar UFs", erro),
+  });
+}
+
+carregarMunicipios(event : MatSelectChange | string){
+  const ufSelecionada = typeof event === 'string' ? event : event.value;
+  this.brasilApiService.listaMunicipios(ufSelecionada).subscribe({
+    next : listaMunicipios => this.municipios = listaMunicipios,
+    error : erro => console.log('ocorreu um erro: ', erro) 
+  })
+}
+
+  salvar() {
+    if(!this.atualizando){
+    this.service.salvar(this.cliente);
+    this.cliente = Cliente.newCliente();
+    this.mostrarMensagem('Salvo com sucesso!');
+    } else { 
+      this.service.atualizar(this.cliente);
+      this.router.navigate(['/consulta']);
+      this.mostrarMensagem('Cadastro atualizado!');
+    }
+  }
+
+  mostrarMensagem(mensagem : string){
+    this.snack.open(mensagem, "Ok");
+  }
+
+}
